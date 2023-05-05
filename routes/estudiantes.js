b8990a3224791f5edf51bdc2024c376b3877569e
BryanMaresDev <BryanMaresDev@outlook.com>
2023-05-05 12:11:03 -0600

    Se añadieron comentarios a las funciones

--- routes/estudiantes.js
+++ routes/estudiantes.js
@@ -2,15 +2,21 @@ const express = require('express');     //Importamos express
 const joi = require('joi');             //Importamos Joi para validar los datos ingresados por el usuario
 const ruta = express.Router();          //Creamos un objeto Router de Express para manejar las rutas
 
+// importamos el objeto importando en eventos.js
 const oEventos = require('./eventos');
+// en este caso sólo necesitamos el array que contiene a los eventos, por lo cual deconstruimos la respuesta y lo asignamos a eventos
 const {eventos} = oEventos;
 
+// array que contendrá a los alumnos creados
 const estudiantes = [];                 
 
+// función para verificar que el estudiante existe dentro del array dado el id
 function existeEstudiante(id) {         
     return (estudiantes.find( estudiante => estudiante.id === parseInt(id)));
 };
 
+// función para validar el email del estudiante que se pretende crear/modificar
+// el parametro post es un valor bool que es true cuando se manda llamar en post y false cuando se manda llamar en put
 function validarEmail(estudiante, post) {
     if(post)
         return !(estudiantes.find( est => est.email === estudiante.email));
@@ -18,6 +24,7 @@ function validarEmail(estudiante, post) {
         return !(estudiantes.find( est => est.email === estudiante.email && est.id === estudiante.id)) 
 }
 
+// función para validar qu elos datos dados por el usuario están correctos
 function validarEstudiante(est) {   
     const schema = joi.object({
         nombre:  joi.string()
@@ -34,12 +41,14 @@ function validarEstudiante(est) {
     return schema.validate(est);
 }
 
+// función para obtener todos los estudiantes creados
 ruta.get('/', (req, res) => {
     if (estudiantes.length == 0)
         return res.status(404).send('No hay alumnos creados')
     res.send(estudiantes);
 });
 
+// función para obtener un estudiante en especifico dado el ID de este
 ruta.get('/:id', (req, res) => {
     const id = req.params.id;
     let estudiante = existeEstudiante(id);
@@ -48,9 +57,11 @@ ruta.get('/:id', (req, res) => {
     res.send(estudiante);
 });
 
+// función para la creación de un estudiante
 ruta.post('/', (req, res) => {
     const {error} = validarEstudiante(req.body);  
 
+    // con esto validamos que el id asignado al nuevo estudiante sea el siguiente al ultimo creado
     let ultimoEstudianteId = 1;
     if(estudiantes.length != 0)
         ultimoEstudianteId = estudiantes[estudiantes.length - 1].id + 1;
@@ -62,6 +73,7 @@ ruta.post('/', (req, res) => {
             email: req.body.email,
             carrera: req.body.carrera,
         };
+        // mandamos llamar la función validar email con parametro post = true
         if (validarEmail(estudiante, true)) {
             estudiantes.push(estudiante);
             return res.send(estudiante);
@@ -71,12 +83,17 @@ ruta.post('/', (req, res) => {
     return res.status(404).send(error);
 });
 
+// función para modificar un alumno dado el id de este
 ruta.put('/:id', (req, res) => {
     let estudiante = existeEstudiante(req.params.id);
     if (!estudiante)
         return res.status(404).send('El estudiante no se encuentra'); 
+    // deconstruimos la respuesta para utilizarlos en diferentes casos
     const {error, value} = validarEstudiante(req.body);
     if (!error) { 
+        // con este bloque validamos que el email que se pretende ingresar existe y que este pertene al
+        // mismo id del estudiante que se pretende modificar, con el parametro post de la función validarEmail
+        // en false
         if (req.body.email && validarEmail(value, false)){ 
             estudiante.nombre = req.body.nombre;
             estudiante.email = req.body.email;
@@ -88,11 +105,13 @@ ruta.put('/:id', (req, res) => {
     res.status(400).send(error);
 });
 
+// eliminamos un estudiante en especifico dado el id
 ruta.delete('/:id', (req, res) => {
     const estudiante = existeEstudiante(req.params.id);
     if (!estudiante) {
         return res.status(404).send('El estudiante no se encuentra');
     }
+    // con la siguiente instrucción eliminamos al estudiante de cualquier evento al que esté inscrito
     eventos.forEach(evento => {
         const index = evento.alumnosInscritos.indexOf(estudiante.id);
         if (index !== -1) {
@@ -104,6 +123,7 @@ ruta.delete('/:id', (req, res) => {
     res.send(estudiante);
 });
 
+// creamos el objeto alumnos
 const objetoAlumnos = {
     ruta,
     estudiantes,
--- routes/eventos.js
+++ routes/eventos.js
@@ -2,14 +2,19 @@ const express = require('express'); //Importamos express
 const joi = require('joi');         //Importamos Joi para validar los datos ingresados por el usuario
 const ruta = express.Router();      //Creamos un objeto Router de Express para manejar las rutas
 
+// Importamos el objeto exportando en estudiantes, para posteriormente utilizarlo
 const alumnos = require('./estudiantes');
 
+// Array que contendrá los evento s creados
 const eventos = [];    
 
+// Función para verificar la existencia de un evento en base al ID
 function existeEvento(id) {                                 
     return (eventos.find( evento => evento.id === parseInt(id)));
 }
 
+// verificamos si exisdte un evento que contenga la misma fecha, hora y lugar del evento que se 
+// desee modificar/crear
 function validarFecha(evento) {    
     const eventoMismaFecha = eventos.find((event) => {
         return event.fecha === evento.fecha && 
@@ -20,16 +25,18 @@ function validarFecha(evento) {
     return !eventoMismaFecha;
 }
 
+// validamos que los datos otorgados por el usuario cumplan con los requerimientos definidos
+// en el schema
 function validarEvento(evento) {    
     const schema = joi.object({
         titulo: joi.string()
                    .min(7)
                    .required(),
         fecha:  joi.string()
-                   .regex(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/)
+                   .regex(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/) // sentencia regex que valida el valor para la fecha con formato yyyy-mm-dd
                    .required(),
         hora:   joi.string()
-                   .regex(/^([01][0-9]|2[0-3]):[0-5][0-9]$/)
+                   .regex(/^([01][0-9]|2[0-3]):[0-5][0-9]$/)                    // sentencia regex para validar formato de fecha en hh:mm
                    .required(),
         lugar:  joi.string()
                    .min(5)
@@ -43,12 +50,14 @@ function validarEvento(evento) {
     return schema.validate(evento);
 }
 
+// función para obtener todos los eventos existentes
 ruta.get('/', (req, res) => {
     if (eventos.length == 0)
         return res.status(404).send('No hay eventos creados');
     res.send(eventos);
 });
 
+// función para obtener un evento en especifico
 ruta.get('/:id', (req, res) => {                   
     const id = req.params.id;                      
     let evento = existeEvento(id);                 
@@ -57,9 +66,11 @@ ruta.get('/:id', (req, res) => {
     res.send(evento);                              
 });  
 
+//función para la creación de un evento
 ruta.post('/', (req, res) => {                      
     const {error} = validarEvento(req.body);  
 
+    // con esto validamos que el id asignado al nuevo evento sea el siguiente al ultimo creado
     let ultimoEventoId = 1;                        
     if(eventos.length != 0)                    
         ultimoEventoId = eventos[eventos.length - 1].id + 1;   
@@ -74,6 +85,7 @@ ruta.post('/', (req, res) => {
             ponente: req.body.ponente,
             alumnosInscritos: []
         };
+        // validamos que no exista otro evento con los mismo valores para fecha, lugar y hora
         if (validarFecha(evento)){                              
             eventos.push(evento);
             res.send(evento);
@@ -83,6 +95,7 @@ ruta.post('/', (req, res) => {
     return res.status(400).send(error);
 });
 
+// función para la edición de un evento dado su ID
 ruta.put('/:id', (req, res) => {
     let evento = existeEvento(req.params.id);
     if (!evento){
@@ -90,6 +103,8 @@ ruta.put('/:id', (req, res) => {
     }
     const {error} = validarEvento(req.body);
     if (!error) {
+        // con la siguiente instrucción verificamos si los camos ingresados son los mismos, dando a entender que sólo se modifó 
+        // el título o el ponente, con lo cual, permitimos que se pueda modificar el evento aún teniendo una fecha ya existente
         const fechaMisma = (evento.fecha === req.body.fecha && evento.hora === req.body.hora && evento.lugar === req.body.lugar);
         if(fechaMisma || validarFecha(req.body)){
             evento.titulo = req.body.titulo,
@@ -106,6 +121,7 @@ ruta.put('/:id', (req, res) => {
     }
 });
 
+// función para eliminar un evento en especifico dado el ID
 ruta.delete('/:id', (req, res) => {
     const evento = existeEvento(req.params.id);
     if (!evento) 
@@ -115,48 +131,59 @@ ruta.delete('/:id', (req, res) => {
     res.send(evento);
 });
 
-/* ----- */
+/* FUNCIONES PARA LA MANIPULACIÓN DE LOS ESTUDIANTES INSCRITOS EN EVENTOSS */
 
+// función para obtener los estudiantes inscritos a un evento dado el ID del evento
 ruta.get('/estudiantesInscritos/:idEvento', (req, res) => {
     if (eventos.length == 0)
         return res.status(404).send('No hay eventos creados aún.');
     const evento = existeEvento(req.params.idEvento);
     if(evento === undefined)
         return res.status(404).send('Este evento no existe.');
+    // obtenemos el array que contiene los ID's de los alumnos inscritos al evento seleccionado
     const alumnosInscritos = eventos[evento.id - 1].alumnosInscritos;
+    // sentencia para verificar si existen alumnos inscritos a este evento
     if (alumnosInscritos.length == 0) {
         return res.status(404).send('No hay alumnos inscritos al evento.');
     }
     res.status(200).send(`ID Evento: ${evento.id} \nID alumnos inscritos:\n ${alumnosInscritos}`);
 });
 
+// función para inscribir estudiantes a eventos dado el id del alumno y del evento
 ruta.post('/estudiantesInscritos/:idEvento/:idEstudiante', (req, res) => {
     const evento = existeEvento(req.params.idEvento);
     const estudiante = alumnos.existeEstudiante(req.params.idEstudiante);
     if(!(evento && estudiante))
         return res.status(404).send('El ID del alumno ó el ID del evento no existen o no son correctos.');
     const alumnosInscritos = eventos[evento.id - 1].alumnosInscritos;
+    // verificamos si es que el estudiante ya está inscrito al evento deseado
     if(alumnosInscritos.includes(estudiante.id))
         return res.status(400).send('El alumno ya está inscrito en el evento.');
+    // si no está inscrito, lo añadimos al array que contiene los id de los alumnos inscritos
     evento.alumnosInscritos.push(estudiante.id);
     res.status(200).send(`ID Evento: ${evento.id} \nID alumnos inscritos:\n ${alumnosInscritos}`);
 });
 
+// función para eliminar a un alumno inscrito a un evento dado el id del alumno y el id del evento
 ruta.delete('/estudiantesInscritos/:idEvento/:idEstudiante', (req, res) => {
     const evento = existeEvento(req.params.idEvento);
     const estudiante = alumnos.existeEstudiante(req.params.idEstudiante);
     if(!(evento && estudiante))
         return res.status(404).send('El ID del alumno ó el ID del evento no existen o no son correctos.');
     const alumnosInscritos = eventos[evento.id - 1].alumnosInscritos;
+    // verificamos si el alumno realmente está inscrito al evento
     if(!alumnosInscritos.includes(estudiante.id))
         return res.status(400).send('El alumno no está inscrito en el evento.'); 
+    // obtenemos el ID dentro del array de alumnos inscritos, para posteriormente eliminarlo
     alumnosInscritos.splice(alumnosInscritos.indexOf(estudiante.id), 1);
     res.status(200).send(`ID Evento: ${evento.id} \nID alumnos inscritos:\n ${alumnosInscritos}`);
 });
 
+// definimos el objet con las funciones ruta como el array eventos, para poder utilizarlo en estudiantes.js
 const objetoEventos = {
     ruta,
     eventos
 };
 
+// exportamos el objeto posteriormente definido
 module.exports = objetoEventos; 
